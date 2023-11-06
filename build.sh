19af3e43b0c6c9174738b3203168282919705c77
pchaos <drifthua@gmail.com>
2023-11-06 18:50:30 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+# Created: 2023-11-06 17:21:54
+# Last Modified: 2023-11-06 17:23:58
+
 mkdir -p SOURCES
 mkdir -p SPECS
 
 cp dingtalk-bin.spec SPECS/
+cp  dingtalk-bin.desktop dingtalk-launcher.sh dingtalk.svg libcairo.so.2 xdg-open README*.md LICENSE SOURCES/
+
+sudo dnf builddep SPECS/dingtalk-bin.spec
+spectool -gR SPECS/dingtalk-bin.spec
+rpmbuild -ba SPECS/dingtalk-bin.spec
