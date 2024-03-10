544d3003415116e77d4981ba2b97892cdc242f9d
pchaos <drifthua@gmail.com>
2024-03-10 14:42:00 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Created: 2023-11-06 17:21:54
-# Last Modified: 2023-11-06 17:23:58
+# Last Modified: 2023-11-28 00:59:42
 
 mkdir -p SOURCES
 mkdir -p SPECS
@@ -11,3 +11,5 @@ cp  dingtalk-bin.desktop dingtalk-launcher.sh dingtalk.svg libcairo.so.2 xdg-ope
 sudo dnf builddep SPECS/dingtalk-bin.spec
 spectool -gR SPECS/dingtalk-bin.spec
 rpmbuild -ba SPECS/dingtalk-bin.spec
+
+echo "sudo dnf install $HOME/rpmbuild/RPMS/x86_64/dingtalk-bin-"
--- dingtalk-bin.spec
+++ dingtalk-bin.spec
@@ -5,7 +5,7 @@
 AutoReqProv: no
 
 Name:           dingtalk-bin
-Version:        7.1.0.31017
+Version:        7.1.0.31218
 Release:        2%{?dist}
 Summary:        钉钉
 
